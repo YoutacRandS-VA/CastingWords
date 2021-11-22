138508e6395f44578fb42d2e670f4b487b5a7184
peterlee <peterlee0127@gmail.com>
2021-11-22 13:52:48 +0800

    Add uuid, Update file save name.

--- router/route.js
+++ router/route.js
@@ -9,7 +9,8 @@ const utils = require('../utils/utils.js');
 const fs = require('fs');
 const multer = require('@koa/multer');
 const upload = multer({ dest: 'uploads/' });
-
+const moment = require('moment');
+const { v4: uuidv4 } = require('uuid');
 /**
  * async checkAuth
  */
@@ -36,10 +37,9 @@ function mapper(_) {
     status: _.status,
     file_name: _.file_name,
     file_size: `${_.file_size}MB`,
-    file_upload_date: _.file_upload_date,
+    file_upload_date: moment(_.file_upload_date).locale('zh_TW').format('LLLL'),
   };
 }
-
 
 route.get('/list', async (ctx) => {
   try {
@@ -67,15 +67,17 @@ route.post(
     let files = ctx.files.file;
     for(let i=0;i<files.length;i++){
       let file = files[i];
-      let originalname = file.originalname;
-      fs.renameSync(`uploads/${file.filename}`, `uploads/${body.title}`);
-      let size = utils.getFileSize(body.title);
+      let originalFileName = file.originalname;
+      let uuid = uuidv4().split("-").pop();
+      let newFileName = `${body.title}_${uuid}_${originalFileName}`;
+      fs.renameSync(`uploads/${file.filename}`, `uploads/${newFileName}`);
+      let size = utils.getFileSize(newFileName);
       const FILE = new db.FILE({
         title: body.title,
         speaker: body.speaker,
         notes: body.notes,
         status: 'upload',
-        file_name: body.title,
+        file_name: newFileName,
         file_size: size,
         file_upload_date: new Date()
       })
